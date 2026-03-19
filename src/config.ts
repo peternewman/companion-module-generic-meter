2a25908a26605c900c5f656e314e1809966e16fd
Peter Newman <peterjnewman@gmail.com>
2026-03-19 00:40:08 +0000

    Remove the unused config fields

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,16 @@
-import { Regex, type SomeCompanionConfigField } from '@companion-module/base'
+import { type SomeCompanionConfigField } from '@companion-module/base'
 
-export interface ModuleConfig {
-	host: string
-	port: number
-}
+export interface ModuleConfig {}
 
 export function GetConfigFields(): SomeCompanionConfigField[] {
 	return [
 		{
-			type: 'textinput',
-			id: 'host',
-			label: 'Target IP',
-			width: 8,
-			regex: Regex.IP,
-		},
-		{
-			type: 'number',
-			id: 'port',
-			label: 'Target Port',
-			width: 4,
-			min: 1,
-			max: 65535,
-			default: 8000,
+			type: 'static-text',
+			id: 'info',
+			width: 12,
+			label: 'Information',
+			value:
+				'This module generates dB meter images from variable values as feedbacks. Please see the help for further setup advice.',
 		},
 	]
 }
